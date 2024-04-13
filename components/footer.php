a9ab9fd8c98f41bcb3b3ae8c0f61c8114bca2a92
Christ Mbuli <121105340+ChristMbuli@users.noreply.github.com>
2024-04-13 04:55:53 +0000

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -4,7 +4,7 @@
                 href="https://portfolio-frontend-three-kappa.vercel.app/" target="_blank">Christ Mbuli</a></p>
 
         <ul class="nav col-md-4 justify-content-end">
-            <li class="nav-item"><a href="cheking.php" class="nav-link px-2 text-body-secondary">Accueil</a></li>
+            <li class="nav-item"><a href="index.php" class="nav-link px-2 text-body-secondary">Accueil</a></li>
             <li class="nav-item"><a href="add-driver.php" class="nav-link px-2 text-body-secondary">Nouveau</a></li>
             <li class="nav-item"><a href="list-driver.php" class="nav-link px-2 text-body-secondary">Liste</a></li>
 
