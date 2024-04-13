443443e236feac0536a646438e02bb29a0804f24
Christ Mbuli <121105340+ChristMbuli@users.noreply.github.com>
2024-04-13 04:38:37 +0000

    Update connexion.php

--- backend/connexion.php
+++ backend/connexion.php
@@ -2,7 +2,7 @@
 
 try {
 
-    $conn = new PDO('mysql:host=localhost;dbname=sinotrack;charset=utf8;', 'root', '');
+    $conn = new PDO('mysql:host=bg5zs2ozur1yedyhjewm-mysql.services.clever-cloud.com;dbname=bg5zs2ozur1yedyhjewm;charset=utf8;', 'uk9nk1j4dbvbyild', 'fa7x8PUj5hPGrfTHr1BF');
 
 } catch (Exception $e) {
     die('Erreur de connexion: ' . $e->getMessage());
